98925dac7c899a29dfedf728304b0e783981f6ed
achaitanyajami <arun.chaitanyajami@news.co.uk>
2020-05-25 00:27:52 +0530

    Testing php files

--- .circleci/bin/phpcs.sh
+++ .circleci/bin/phpcs.sh
@@ -6,6 +6,7 @@ PROJECT=`php -r "echo dirname(dirname(dirname(realpath('$0'))));"`
 STAGED_FILES_CMD=`git diff --cached --name-only --diff-filter=ACMR HEAD | grep \\\\.php`
 
 echo "$PROJECT"
+echo `pwd`
 
 # Determine if a file list is passed
 if [ "$#" -eq 1 ]
